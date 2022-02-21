8e2fd38bfee35f9dbb5f3024d71342c39d831110
unknown <jlmv84Qgmail.com>
2022-02-21 08:19:57 -0300

    indecision app

---
+++ vue03/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <indecision />
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <Counter :start="101" /> -->
+</template>
+
+<script>
+// import Counter from '@/components/Counter';
+import Indecision from '@/components/Indecision.vue';
+export default {
+  name: 'App',
+  components: {
+    // Counter,
+    Indecision
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
